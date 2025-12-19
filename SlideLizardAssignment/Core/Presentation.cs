d8cb9a89d6e0b65420c92cbaca522acd406ee9dc
FilipKaleskov <f.kaleskov@students.htl-leonding.ac.at>
2025-12-19 08:47:51 +0100

    Presentation and Repo and DataSeeding done

--- SlideLizardAssignment/Core/Presentation.cs
+++ SlideLizardAssignment/Core/Presentation.cs
@@ -2,5 +2,26 @@ namespace Core;
 
 public class Presentation
 {
-    
+    public string Name { get; set; }
+    public DateTime FromDate { get; set; }
+    public DateTime ToDate { get; set; }
+    public string Location { get; set; }
+
+    protected bool Equals(Presentation other)
+    {
+        return Name == other.Name;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is null) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        if (obj.GetType() != GetType()) return false;
+        return Equals((Presentation)obj);
+    }
+
+    public override int GetHashCode()
+    {
+        return Name.GetHashCode();
+    }
 }
--- SlideLizardAssignment/Core/PresentationRepository.cs
+++ SlideLizardAssignment/Core/PresentationRepository.cs
@@ -2,5 +2,25 @@ namespace Core;
 
 public class PresentationRepository
 {
-    
+    private readonly List<Presentation> presentations = DataSeeding.GetData().ToList();
+
+    public void AddPresentation(Presentation presentation)
+    {
+        if (presentations.Contains(presentation))
+        {
+            throw new InvalidOperationException("Presentation already exists.");
+        }
+        
+        presentations.Add(presentation);
+    }
+
+    public IReadOnlyCollection<Presentation> GetAllPresentations()
+    {
+        return presentations;
+    }
+
+    public int GetPresentationsInTimeStamp(DateTime date)
+    {
+        return presentations.Count(p => p.FromDate <= date && p.ToDate >= date);
+    }
 }
